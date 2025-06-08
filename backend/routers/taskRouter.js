ce371e5a1f811d4ff62f84fbd74dbef9a2b668f3
Andrej <asumanovski@yahoo.com>
2025-06-08 14:58:46 +0200

    Made fetching od todays task

--- backend/routers/taskRouter.js
+++ backend/routers/taskRouter.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const taskController = require('../controllers/taskController');
 
-router.get('/task/:date', taskController.getTaskByDate);
-router.put('/task/:id', taskController.updateAttemptsTask);
-router.put('/task/:id', taskController.updateSolvedTask);
+router.get('/:date', taskController.getTaskByDate);
+router.put('/:id/attempts', taskController.updateAttemptsTask);
+router.put('/:id/solved', taskController.updateSolvedTask);
+
+module.exports = router;
