479eacad01c8926648a1e9ec1bf93b97825a38ae
Andrej <asumanovski@yahoo.com>
2025-06-09 15:29:56 +0200

    Made leaderboard fetching function and frontend call

--- client/src/LandingPage/LandingPage.jsx
+++ client/src/LandingPage/LandingPage.jsx
@@ -4,18 +4,19 @@ import VisionSection from "./components/VisionSection";
 import Footer from "./components/Footer";
 import Intro from "./components/Intro";
 import NavbarLanding from "./components/NavbarLanding";
-import LeaderLanding from "./components/LeaderLanding";
+import LeaderBoardEx from "@/LandingPage/components/LeaderBoardEx.jsx";
+
 const LandingPage = () => {
-  return (
-    <div data-theme="luxury">
-      <NavbarLanding />
-      <Hero />
-      <Intro />
-      <VisionSection />
-      <LeaderLanding />
-      <Footer />
-    </div>
-  );
+    return (
+        <div data-theme="luxury">
+            <NavbarLanding/>
+            <Hero/>
+            <Intro/>
+            <VisionSection/>
+            <LeaderBoardEx/>
+            <Footer/>
+        </div>
+    );
 };
 
 export default LandingPage;
