2a723567d3a4230b5708031200cd63cd3ab2e474
Dimitar Arsov <dimitararsov04@gmail.com>
2025-06-22 19:44:27 +0200

    use malcew

--- client/src/LandingPage/components/Hero.jsx
+++ client/src/LandingPage/components/Hero.jsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import hero from '../../assets/images/hero-bg.jpg';
-import {  useNavigate } from 'react-router-dom';
+import React from "react";
+import hero from "../../assets/images/hero-bg.jpg";
+import { useNavigate } from "react-router-dom";
 
 const Hero = () => {
-  const user = JSON.parse(localStorage.getItem('user'));
+  const user = JSON.parse(localStorage.getItem("user"));
   const navigate = useNavigate();
   return (
     <div
@@ -15,23 +15,21 @@ const Hero = () => {
       <div className="hero-overlay "></div>
       <div className="hero-content text-neutral-content text-center bg-black/70 p-20 rounded-lg">
         <div className="w-full">
-          {!user && (
-              <h1 className="mb-5 text-6xl font-bold">FINKI RANKED</h1>
-          )}
+          {!user && <h1 className="mb-5 text-6xl font-bold">FINKI RANKED</h1>}
           {user && (
-              <h1 className="mb-5 text-4xl font-bold">Welcome back, {user.username}</h1>
+            <h1 className="mb-5 text-4xl font-bold">
+              Welcome back, {user.username}
+            </h1>
           )}
 
-          <p className="mb-5">
-          Daily coding challenges and competitions for FINKI students
-          </p>
+          <p className="mb-5">Daily coding challenges for FINKI students</p>
           {!user && (
-            <a className="btn btn-xl" onClick={() => navigate('/login')}>
+            <a className="btn btn-xl" onClick={() => navigate("/login")}>
               Join now
             </a>
           )}
           {user && (
-            <a className="btn btn-xl" onClick={() => navigate('/dashboard')}>
+            <a className="btn btn-xl" onClick={() => navigate("/dashboard")}>
               Solve today's challenge
             </a>
           )}
