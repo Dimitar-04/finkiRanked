91d4df204a816c5b48529e0fb589c7a339fd2abe
Dimitar Arsov <dimitararsov04@gmail.com>
2025-07-01 17:15:52 +0200

    Cleanup

--- backend/models/Challenge.js
+++ backend/models/Challenge.js
@@ -1,8 +1,8 @@
 class Challenge {
   constructor(data = {}) {
     this.id = data.id;
-    this.title = data.title || '';
-    this.content = data.content || '';
+    this.title = data.title || "";
+    this.content = data.content || "";
     this.solving_date = data.solving_date;
     this.attempted_by = data.attempted_by;
     this.solved_by = data.solved_by;
